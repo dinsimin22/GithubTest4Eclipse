3bcc73247efed5b0a909c7935266b14facbd2ead
simin.ding <simin.ding@chinacreator.com>
2019-07-15 14:37:52 +0800

    更新一个文件

--- githubtest/src/main/java/com/chinacreator/test/githubtest/App.java
+++ githubtest/src/main/java/com/chinacreator/test/githubtest/App.java
@@ -1,13 +1,13 @@
 package com.chinacreator.test.githubtest;
 
 /**
- * Hello world!
+ * Hello world version 0.2 !
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Second Hello World!" );
     }
 }
